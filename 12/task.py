5aa28adc1bcfefa956d0cc225e6507436274bc49
IrinaSavosh <irina_savosh@mail.ru>
2023-02-03 23:29:14 +0300

    Task 14

--- 12/task.py
+++ 12/task.py
@@ -4,19 +4,29 @@
 # этого Петя делает две подсказки. Он называет сумму этих чисел S и их
 # произведение P. Помогите Кате отгадать задуманные Петей числа.
 
-from random import randint
-n=int(input())
-a=[randint(10,12) for i in range(n)]
-print(a)
-max1=1
-k=1
-for i in range(1,n):
-    if a[i]==a[i-1]:
-        k+=1
-    else:
-        if k>=max1:
-            max1=k
-        k=1
-if k>max1:
-    max1=k
-print(max1)
+
+
+
+
+
+
+
+
+
+
+# from random import randint
+# n=int(input())
+# a=[randint(10,12) for i in range(n)]
+# print(a)
+# max1=1
+# k=1
+# for i in range(1,n):
+#     if a[i]==a[i-1]:
+#         k+=1
+#     else:
+#         if k>=max1:
+#             max1=k
+#         k=1
+# if k>max1:
+#     max1=k
+# print(max1)
