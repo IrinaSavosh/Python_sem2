deeb8d1793bdc428b989d56c60a7150661fc3afe
IrinaSavosh <irina_savosh@mail.ru>
2023-02-04 23:14:23 +0300

    Task 12

--- 12/task.py
+++ 12/task.py
@@ -4,6 +4,35 @@
 # этого Петя делает две подсказки. Он называет сумму этих чисел S и их
 # произведение P. Помогите Кате отгадать задуманные Петей числа.
 
+# 4 4 -> 2 2
+# 5 6 -> 2 3
+
+
+import random
+
+x = random.randint(0, 1000)
+y = random.randint(0, 1000)
+sum = x+y
+prod = x*y
+print(f"Петя загадал числа, произведение которых равно {prod}, а сумма - {sum}")
+i = j = 0
+for i in range(1001):
+   for j in range(1001):
+      if i+j == sum and i*j == prod:
+         x = i
+         y = j
+      else:
+         j += 1
+   i += 1
+
+print(f"Катя назвала числа, которые загадал Петя. Первое число равно {x}, а второе - {y}")
+# a, b = map(int, input().split())
+# res = []
+# for i in range(a + b):
+#     if i == (a * i - b)**0.5:
+#         res.append(i)
+# print(*res if len(res) == 2 else res + res)
+
 
 
 
